e31d5d833ca55cf895bc83440bb6e3e98f920a9f
jayesd3v <jayesd3v@gmail.com>
2021-07-23 01:59:05 +0700

    first commit

---
+++ src/main/java/fpt/chinht2/jenkins/test/TestApplication.java
@@ -0,0 +1,13 @@
+package fpt.chinht2.jenkins.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fpt/chinht2/jenkins/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.chinht2.jenkins.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
