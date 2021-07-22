a3b030d536edcb95f7f547fd3d4b1a6302478f41
jayesd3v <jayesd3v@gmail.com>
2021-07-23 02:17:20 +0700

    modify asdf controller

--- src/main/java/fpt/chinht2/jenkins/test/TestApplication.java
+++ src/main/java/fpt/chinht2/jenkins/test/TestApplication.java
@@ -2,9 +2,17 @@ package fpt.chinht2.jenkins.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
+
+	@GetMapping("/")
+	public String asdf() {
+		return "Asdf1";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
